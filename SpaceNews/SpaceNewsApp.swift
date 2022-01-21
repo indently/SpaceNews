14f690f838f3ad0745d40e0316a99b4e1605141b
Code Palace <codepalace@fastmail.se>
2022-01-21 10:12:00 +0100

    Initial Commit

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by Federico on 21/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
