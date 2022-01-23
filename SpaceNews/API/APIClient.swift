83bc799d1e6fcaac284b5490219dcf6aa8355a8c
Code Palace <codepalace@fastmail.se>
2022-01-23 09:37:31 +0100

    Added error handling in case there's no internet

--- SpaceNews/API/APIClient.swift
+++ SpaceNews/API/APIClient.swift
@@ -25,10 +25,17 @@ struct SpaceData : Codable, Identifiable {
         guard let url = URL(string: "https://api.spaceflightnewsapi.net/v3/articles?_limit=10") else { return }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else { return }
+            // Check if there's data, otherwise return an error object
+            guard let data = data else {
+                let tempError = error!.localizedDescription
+                DispatchQueue.main.async {
+                    self.news = [SpaceData(id: 0, title: tempError, url: "Error", imageUrl: "Error", newsSite: "Error", summary: "Try swiping down to refresh as soon as you have internet again.", publishedAt: "Error")]
+                }
+                return }
             
             let spaceData = try! JSONDecoder().decode([SpaceData].self, from: data)
             
+            // Update the list to the latest data
             DispatchQueue.main.async {
                 print("Loaded new data successfully! Articles: \(spaceData.count)")
                 self.news = spaceData
